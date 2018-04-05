7e56350e16160928244b931f487abc8ab0c6fc36
candidato7 <fideljose1708@hotmail.com>
2018-04-05 18:42:33 -0500

    css3 animation and transaction

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -13,7 +13,7 @@ import { HomeComponent } from './home/home.component';
 const rutas: Routes = [
 	// Ojo al componente principal(en este caso Home) se le debe colocar '<router-outlet></router-outlet>'
 	// para que redireccione corractamente a la pagina hija.
-	{path: "principal", component: HomeComponent, 
+	/*{path: "principal", component: HomeComponent, 
 		children: [
 			{path: "padre", component: PadreComponent},
 			{path: "hijo", component: HijoComponent},
@@ -21,16 +21,16 @@ const rutas: Routes = [
 			{path: "auto", component : AutoComponent},
 			{path: "contacto", component : ContactoClienteComponent}
 		]
-	}
+	}*/
 
-	/*{path: "inicio", component: HomeComponent},
+	{path: "inicio", component: HomeComponent},
 	{path: "padre", component: PadreComponent},
 	{path: "hijo", component: HijoComponent},
 	{path: "", redirectTo: "inicio", pathMatch: "full"},
 	{path: "almacen", component : AlmacenComponent},
 	{path: "auto", component : AutoComponent},
 	{path: "contacto", component : ContactoClienteComponent},
-	{path: "**", component: Page404Component}*/
+	{path: "**", component: Page404Component}
 ];
 
 export const appRoutingProviders: any[]= [];
